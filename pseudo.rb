05a6ae5296f5ce038b83a4f026e7299d306f2f22
codea_mac_04 <codea_mac_04@gmail.com>
2017-05-26 14:52:12 -0500

    pseudo

--- pseudo.rb
+++ pseudo.rb
@@ -1,33 +1,3 @@
-students = [[["Eliezer", 7], ["Mariam", 7], ["Deborah", 10], ["Harley", 7]],
- [["D'angelo", 10], ["Brittany", 8], ["Hubert", 7], ["Erling", 9]],
- [["Bradford", 7], ["Theodora", 10], ["Casandra", 9], ["Lindsay", 8]],
- [["Hilma", 10], ["Lina", 10], ["Isabell", 8], ["Urban", 9]]]
-
-def best_students(students)
-  best_students = []
-  x, y        = 0, 0
-
-   while y < students.length
-    current_student  = students[y][x]
-
-    if current_student[1] == 10
-      best_students.push(current_student[0])
-    end
-
-    if x == (students[y].length - 1)
-      x = 0
-      y += 1
-    else
-      x += 1
-    end
-  end
-  best_students
-end 
-
-p best_students(students) == ["Deborah","D'angelo","Theodora","Hilma","Lina"]
-
-=begin
-
 inicioMetodo best_students(students)
     best_students = []
     x, y = 0, 0
@@ -53,8 +23,8 @@ students = [[["Eliezer", 7], ["Mariam", 7], ["Deborah", 10], ["Harley", 7]],
  [["Bradford", 7], ["Theodora", 10], ["Casandra", 9], ["Lindsay", 8]],
  [["Hilma", 10], ["Lina", 10], ["Isabell", 8], ["Urban", 9]]]
 
-
 CALL best_students(students)
+
 finPrograma
 
 
